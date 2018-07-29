ca9b87e1c18e85ee21b0ce3461faeaa712a23ff6
Reza R <rastegar8@gmail.com>
2018-07-29 15:10:29 -0500

    codes updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// dependencies, install > npm install mysql to use the function below for useing node to manipulate the database
+var mysql = require("mysql"); // run npm install mysql for this one to work
+var inquirer = require("inquirer"); // run npm install inquirer for this one to work
 
-var connection = mysql.createConnection({
+// Establishing a connection with 5 required components
+var connection = mysql.createConnection({ // this is to create the connection
     host: "localhost",
     port: 3306,
     user: "root",
@@ -9,19 +11,21 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.connect(function (error)) {
-    if (error) throw error;
+connection.connect(function (err) { // this is to connect via the  created  connection
+    if (err) throw err;
     console.log(" You are connected as id " + connection.threadId + "\n");
     runBamazon();
-};
+});
 
 function runBamazon() {
     inquirer.prompt({
-        name: "action",
+        name: "items",
         type: "list",
-        message: "message to user goes here",
+        message: "Below is the list of all available items with price and available quantity",
         chioces: [
         ]
+    }).then(function (answer) {
+        var query = "SELECT * FROM bamazondb.products"
     })
 }
 
